96859b78928248ee3fd41e5c5f43778125038c50
tony.astolfi <tony.astolfi@tamr.com>
2020-06-05 10:19:58 -0400

    WIP

--- src/hittop/parent_task.hpp
+++ src/hittop/parent_task.hpp
@@ -3,6 +3,16 @@
 #define HITTOP_PARENT_TASK_HPP
 
 namespace hittop {
+
+class ParentTask<typename Derived>
+{
+  /*
+TODO:
+- async_run
+- off_task(deferred_work)
+   */
+}
+
 }  // namespace hittop
 
 #endif  // HITTOP_PARENT_TASK_HPP
--- src/hittop/serial_task.hpp
+++ src/hittop/serial_task.hpp
@@ -3,6 +3,12 @@
 #define HITTOP_SERIAL_TASK_HPP
 
 namespace hittop {
+/*
+TODO:
+on_error
+async_run
+ */
+
 }  // namespace hittop
 
 #endif  // HITTOP_SERIAL_TASK_HPP)
